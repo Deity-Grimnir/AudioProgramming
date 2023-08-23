d384104719ab13e36a8d6083a31e93876e0848f7
Deity-Grimnir <96482271+Deity-Grimnir@users.noreply.github.com>
2023-08-23 12:06:54 +0200

    added first part of custom knobs for eq

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,12 +11,43 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomVerticalSlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomVerticalSlider() : juce::Slider(juce::Slider::SliderStyle::Rotary,juce::Slider::NoTextBox)
-    {
+    void drawRotarySlider (juce::Graphics&,
+        int x, int y, int width, int height,
+        float sliderPosProportional,
+        float rotaryStartAngle,
+        float rotaryEndAngle,
+        juce::Slider&) override {}
+};
 
+
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+        juce::Slider(juce::Slider::SliderStyle::Rotary,
+            juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap),
+        suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf); 
     }
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+
+    void paint(juce::Graphics& g) override {};
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const { return 14; }
+    juce::String getDisplayString() const;
+private:
+    LookAndFeel lnf;
+
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
+
+
 };
 struct ResponseCurveComponent : juce::Component,
     juce::AudioProcessorParameter::Listener,
@@ -60,7 +91,7 @@ private:
 
 
     
-    CustomVerticalSlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, HighCutSlopeSlider;
+    RotarySliderWithLabels peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, HighCutSlopeSlider;
 
     ResponseCurveComponent responseCurveComponent;
 
